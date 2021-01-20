eb1b5084aa9c11ea434e765ed38697eb4d577bbd
wes <Wesleymiller1996@gmail.com>
2021-01-19 19:33:42 -0600

    fixed time formatting, styled home page

--- MyHome/wwwroot/site.js
+++ MyHome/wwwroot/site.js
@@ -1,12 +1,16 @@
 function initializeContent() {
     var refreshRate = 1000;
-    var date = new Date();
 
-    document.getElementById('main-time').innerHTML = date.getHours() + ":" + date.getMinutes();
+    updateTime();
     setInterval(updateTime, refreshRate);
+    setInterval(updateBackground, refreshRate * 300);
 }
 
 function updateTime() {
     var date = new Date();
-    document.getElementById('main-time').innerHTML = date.getHours() + ":" + date.getMinutes();
+    document.getElementById('main-time').innerHTML = date.getHours() + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
+}
+
+function updateBackground() {
+    document.getElementById('main-image').style.backgroundImage = "url('https://source.unsplash.com/random?sig="+  Math.random() +"')";
 }
