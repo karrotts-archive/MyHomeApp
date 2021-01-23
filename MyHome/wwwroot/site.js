aea461c7d32d2e84ad64d0bcd48313c8d95298f5
wes <Wesleymiller1996@gmail.com>
2021-01-22 22:34:01 -0600

    corrected UI issue, smoothed background transitions.

--- MyHome/wwwroot/site.js
+++ MyHome/wwwroot/site.js
@@ -1,9 +1,15 @@
+var nextImage;
+var lockTransition;
+
 function initializeContent() {
     var refreshRate = 1000;
 
+    nextImage = preloadImage("https://source.unsplash.com/random?sig=" + Math.random());
+    lockTransition = false;
+
     updateTime();
     setInterval(updateTime, refreshRate);
-    setInterval(updateBackground, refreshRate * 300);
+    setInterval(transitionBackground, refreshRate * 5);
 }
 
 function updateTime() {
@@ -11,6 +17,28 @@ function updateTime() {
     document.getElementById('main-time').innerHTML = date.getHours() + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes());
 }
 
+var mainImageBlock = document.querySelector('.main-image-block');
+mainImageBlock.addEventListener('transitionend', function () {
+    if (!lockTransition) {
+        lockTransition = !lockTransition;
+        updateBackground();
+    } else {
+        lockTransition = !lockTransition;
+    }
+});
+
+function transitionBackground() {
+    document.getElementById('main-image-block').classList.add("main-image-transition");
+}
+
+function preloadImage(url) {
+    var img = new Image();
+    img.src = url;
+    return img;
+}
+
 function updateBackground() {
-    document.getElementById('main-image').style.backgroundImage = "url('https://source.unsplash.com/random?sig="+  Math.random() +"')";
+    document.getElementById('main-image').style.backgroundImage = "url("+ nextImage.src +")";
+    document.getElementById('main-image-block').classList.remove("main-image-transition");
+    nextImage = preloadImage("https://source.unsplash.com/random?sig=" + Math.random());
 }
