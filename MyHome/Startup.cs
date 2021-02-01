af517d68a813c2f4205b4f3fd4d2584948f1a09e
Wes B. Miller <WesleyMiller1996@gmail.com>
2021-01-31 19:48:57 -0600

    Update Startup.cs

--- MyHome/Startup.cs
+++ MyHome/Startup.cs
@@ -27,7 +27,6 @@ namespace MyHome
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
